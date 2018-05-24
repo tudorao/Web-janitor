ccaffade1ce0d6925122e1152dcaf413505095b8
tudorao <tudorao@yahoo.com>
2018-05-24 15:39:01 +0200

    404 user acces

--- theme/www/test.php
+++ theme/www/test.php
@@ -1,13 +1,15 @@
 
 <?php
-// $access_item = false;
-// if(isset($read_access) && $read_access) {
-// 	return;
-// }
+// $access_item["/"]= true;
+$access_item["/"] = false; $access_item["/test1"] = true; $access_item["/test2"] = true;
+ if(isset($read_access) && $read_access) {
+ 	return;
+ }
+//$access_item["/"] = false; $access_item["/test1"] = true;
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 $action = $page->actions();
-$page->bodyClass("sarah");
-$page->pageTitle("Sarah's template");
+$page->bodyClass("Octavian");
+$page->pageTitle("Octavian's template");
 if(count($action) == 1 && $action[0] == "test1") {
 	$page->page(array(
 		"templates" => "pages/testPage1.php"
